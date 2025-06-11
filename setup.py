0206084c82eef8c4c220c626c525c27e1e91176f
yinchu <sales@pytorchsoft.com>
2025-06-11 00:57:06 -0700

    bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="xlpd",
-    version="0.1.0",
+    version="0.1.1",
     author="Yinchu Xia",
     author_email="sales@pytorchsoft.com",
     description="xiu lian ping de",
