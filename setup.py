69c7ff1c89e0c249684aa4fc77ab85f91a486c9c
yinchu <sales@pytorchsoft.com>
2025-06-11 00:31:55 -0700

    setup

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="xlpd",
+    version="0.1.0",
+    author="Yinchu Xia",
+    author_email="sales@pytorchsoft.com",
+    description="xiu lian ping de",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/pytorchsoft/xlpd",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",  # List your dependencies here
+        "requests",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
